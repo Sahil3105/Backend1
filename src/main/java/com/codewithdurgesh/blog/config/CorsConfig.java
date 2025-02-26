2686d58e92a3517dfeca8aca0283f68de9eb5646
Sahil3105 <alhatsahil934@gmail.com>
2025-02-26 10:31:39 +0530

    Update CorsConfig.java

--- src/main/java/com/codewithdurgesh/blog/config/CorsConfig.java
+++ src/main/java/com/codewithdurgesh/blog/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOriginPatterns("http://18.208.211.74:3000") // Explicit Frontend URL
+                    .allowedOriginPatterns("http://34.228.157.161:3000") // Explicit Frontend URL
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
