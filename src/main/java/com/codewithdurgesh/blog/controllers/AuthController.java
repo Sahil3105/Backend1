a9dfa53ce878e3dea7b2f74138249d7a357f7b6e
Sahil3105 <alhatsahil934@gmail.com>
2025-02-26 10:32:03 +0530

    Update AuthController.java

--- src/main/java/com/codewithdurgesh/blog/controllers/AuthController.java
+++ src/main/java/com/codewithdurgesh/blog/controllers/AuthController.java
@@ -33,7 +33,7 @@ import com.codewithdurgesh.blog.services.UserService;
 
 @RestController
 @RequestMapping("/api/v1/auth/")
-@CrossOrigin(origins = "http://18.208.211.74:3000", allowCredentials = "true")
+@CrossOrigin(origins = "http://34.228.157.161:3000", allowCredentials = "true")
 public class AuthController {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
